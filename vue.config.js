e2e8e6e4f6c3a2ca2b82ca0a7f44339b00292ed7
nicsonxiao <nicsonxiao@163.com>
2022-04-05 22:09:18 +0800

    配置跨域参数和配置全局样式

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,30 @@
 module.exports = {
+    lintOnSave:false, //关闭lints检测警告
+    devServer:{
+        compress:false,
+        open:true,
+        proxy:{
+            '/path':{ // 这个意思是：原先以 /api 开头的请求
+                //代理的具体地址
+                target:'',
+                //是否开启代理,是否允许跨域
+                changeOrigin:true,
+                //ws
+                //重写
+                pathRewrite:{
+                    '/path':'/' // 上面那个 /path 替换成这个/ 
+                }
+            }
+        }
+    },
+    css:{
+        loadderOption:{
+            //设置全部配置
+            sass:{
+                prependData:`@import "./src/assets/common/css/global.scss"`
+            }
+        },
+    },
     publicPath : "/",
     assetsDir:"static/public"
-   }
+}
